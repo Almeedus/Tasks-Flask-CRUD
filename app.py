5e4cb57b386850610784364d9debb23532853b71
Eduardo Almeida <almeedusa@gmail.com>
2024-02-07 19:22:33 -0300

    create a GET route to show tasks & class Task

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
+app =  Flask(__name__)
 
-# __name__ = "__main__"
-app = Flask(__name__)
+tasks = []
+tasks_id_control = 1
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global tasks_id_control
+    data = request.get_json()
+    new_task = Task(id=tasks_id_control, title=data['title'], description= data.get('description', ''))
+    tasks_id_control += 1
+    tasks.append(new_task)
+    return jsonify({'message': 'Nova tarefa criada com sucesso.'})
 
-#criando rota = comunicação com cliente(usuario)
-@app.route("/")
-def hello_world():
-    return f'Hello World'
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks_list = []
+    tasks_total = 0
+    
+    for task in tasks:
+        tasks_list.append(task.to_dict())
+        tasks_total += 1
 
-@app.route("/about")
-def sobre():
-    return f'Pagina sobre programação.'
+    output = {
+        "tasks": tasks_list,
+        "total_tasks": tasks_total
+    }
+
+    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
