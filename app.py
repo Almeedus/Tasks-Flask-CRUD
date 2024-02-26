16722ea291a7b10bcb13340075174f426de90c8e
Eduardo Almeida <almeedusa@gmail.com>
2024-02-26 15:42:22 -0300

    Create CRUD tasks

--- app.py
+++ app.py
@@ -24,10 +24,52 @@ def get_tasks():
 
     output = {
         "tasks": tasks_list,
-        "total_tasks": tasks_total
+        "total_tasks": len(tasks_list)
     }
 
     return jsonify(output)
+
+#permite que receba na rota um parametro
+@app.route('/tasks/<int:id>', methods=['GET']) #parametro de rota
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+        
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+#nessa rota recebemos tanto o parametro na rota quanto um corpo de requisição
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = None
+    for t in tasks:
+        if t.id == task_id:
+            task = t
+            break
+
+    if task == None:
+        return jsonify({'message': "Não foi possível encontrar a atividade"}), 404
+    
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    return jsonify({'message': 'Tarefa atualizada com sucesso'})
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = None
+    for t in tasks:
+        if t.id == task_id:
+            task = t
+            break
+    
+    if task == None:
+        return jsonify({'message': "Não foi possível encontrar a atividade"}), 404
+    
+    tasks.remove(task)
+    return jsonify({'message': 'Tarefa deletada com sucesso.'})
+            
 
 if __name__ == "__main__":
     app.run(debug=True)
